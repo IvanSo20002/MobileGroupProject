c51458f527ae2f6c513e4799a4f80f390087960c
IanFong062 <106955687+IanFong062@users.noreply.github.com>
2022-11-24 13:52:45 +0800

    shopping cart update

---
+++ app/src/main/java/ViewHolder/CartAdapter.java
@@ -0,0 +1,17 @@
+package ViewHolder;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+class CartViewHolder extends RecyclerView.ViewHolder implements View. OnClickListener{
+    public CartViewHolder (View itemView) {
+        super(itemView);
+    }
+        @Override
+        public void onClick (View view){
+
+        }
+    }
+        public class CartAdapter{
+    }
